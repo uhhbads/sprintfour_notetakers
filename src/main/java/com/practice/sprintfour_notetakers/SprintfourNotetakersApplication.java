cce06133106a7d6c0d43f120b44920a9add8e456
uhhbads <abadshoodee@gmail.com>
2026-02-05 00:57:04 +0800

    init

---
+++ src/main/java/com/practice/sprintfour_notetakers/SprintfourNotetakersApplication.java
@@ -0,0 +1,13 @@
+package com.practice.sprintfour_notetakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintfourNotetakersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintfourNotetakersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/sprintfour_notetakers/SprintfourNotetakersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.sprintfour_notetakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintfourNotetakersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

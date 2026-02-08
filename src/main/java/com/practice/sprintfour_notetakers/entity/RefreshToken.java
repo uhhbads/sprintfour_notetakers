7efee60af8228aa29f187b5ef43e36ee6f98ea41
uhhbads <abadshoodee@gmail.com>
2026-02-08 22:56:14 +0800

    [entities] added table name

--- src/main/java/com/practice/sprintfour_notetakers/entity/RefreshToken.java
+++ src/main/java/com/practice/sprintfour_notetakers/entity/RefreshToken.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
+@Table(name = "refresh token")
 public class RefreshToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
